1bd3e5bfc29461e741bf1bd1d251c9c82d0710fa
Jack Xu <jack19980118@gmail.com>
2016-11-17 16:17:46 -0500

    robotic arm sketch

---
+++ RobotArmRC.c
@@ -0,0 +1,25 @@
+typedef struct{
+	float x;
+	float y;
+	float z;
+	float t;
+	bool validility;
+} Point;
+
+const int MAX=20;
+task main()
+{
+
+//ReadFile
+	Point pos[MAX];
+	for(int i=0;i<MAX;i++){
+		pos[i]=readNextPoint();
+	}
+}
+
+Point readNextPoint(){
+	Point coor;
+
+
+	return coor;
+}
