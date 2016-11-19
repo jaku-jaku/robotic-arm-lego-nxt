fda4184e04e0bbdc91339d31402d232d74cc9fd8
Jack Xu <jack19980118@gmail.com>
2016-11-18 21:47:52 -0500

    UPDATED_VERSION_OZY_CODE_MERGED
    ADD HEADER TO THE FILE

--- RobotArmRC.c
+++ RobotArmRC.c
@@ -1,29 +1,133 @@
-//library
-#include "NXTServo-lib-UW.c"
-//Constant
-const int SColor=S1;
-const int SUltra=S2;
-const int STouch=S3;
-const int SServo=S4;
-const int Joint1=motorA;
-const int Joint2=1;
-const int Joint3=2;
-const int Gripper=3;
+/*+-----------------------------------------------------------------------------
+
+																ROBOTIC ARM
+															---------------
+					  			TEAM JADO - MTE100 && GENE Final Project
+
+ |+------------------------------------------------------------------------------
+ ||
+ ||           	Team:  		JADO   #51
+ ||
+ ||         	Author:  Jack(Jianxiang) Xu
+ ||										 Ali Toyserkani
+ ||										 Dustin Hu
+ ||										 Oswaldo Ferro Hernandez
+ ||
+ ||        	Project:  Robotic Arm
+ ||
+ ||	 	 	Description:  ROBOTC
+ ||
+ |+------------------------------------------------------------------------------
+ ||
+ ||        	Purpose:  [Main class description]
+ ||
+ ||    	 Interfaces:    1. Auto-Calibration
+ || 										2. Beeping sound to give a feedback for user
+ ||											3. Safety always first, Ultrasonic Sensor to make
+ ||											sure workplace safe.
+ ||
+ |+------------------------------------------------------------------------------
+ ||
+ ||	BASIC_WORK_FLOW:  AUTO-CALIBRATION -> READ_POINTS_FROM_FILE -> CHECK_BOUNDARY
+ ||										 -> MASSAGE_IT!!! -> CHECK_VALIDITY -> OUTPUT_TO_MOTOR
+ ||
+ ||   	  Constants:  (NAME + BRIEF_DESCRIPTION)
+ ||
+ ||    	 	 Function:  [List the names and arguments of all defined
+ ||                   constructors.]
+ ||
+ ++------------------------------------------------------------------------------*/
+
+#include "definitions.c"
+
+//Define the output Port
+const int S_COLOR=S1;
+const int S_ULTRA=S2;
+const int S_TOUCH=S3;
+const int S_SERVO=S4;
+const int JOINT1=motorA;
+const int JOINT2=1;
+const int JOINT3=2;
+const int GRIPPER=3;
+//Size of data parsed at once
 const int MAX=20;
-//Point Obj
-typedef struct{
-	float x;
-	float y;
-	float z;
-	float t;
-} Point;
-typedef struct{
-	float alpha;
-	float beta;
-	float thelta;
-	float t;
-} Angle;
-//ReadCoordinateFromData
+const int FULLROTATION_EC_VALUE = 360*5;
+
+//----------------------//
+//---AUTO-CALIBRATION---//
+//------zero axis-------//
+//----------------------//
+int getECValue (){
+	return nMotorEncoder [JOINT1];
+}
+
+void zeroECValue (){
+	nMotorEncoder [JOINT1] = 0;
+}
+
+int getDistance (){
+	return SensorValue [S_ULTRA];
+}
+
+//THIS HAS TO CHANGE!! USE THE FUNCTION THAT MOVES THE MOTORS!! moveToTarget() IS A PROVISIONAL FUNCTION
+void rotate (bool clockwise, int power){
+	if (!clockwise)
+		power*=-1;
+	motor [JOINT1] = power;
+}
+
+void moveToTarget (int targetEC, int tolerance){	//Provisional function for proof of concept
+	int moveSpeed = 75;
+	//sigmoid function , PD control
+	bool cw  = true;
+	if (targetEC > FULLROTATION_EC_VALUE /2.0){
+		cw = false;
+		targetEC= FULLROTATION_EC_VALUE /2.0-targetEC;
+	}
+
+	rotate (cw, moveSpeed);
+
+	while ((fabs(getECValue())-fabs(targetEC)) <= tolerance);
+
+	rotate (false, 0);
+	//It will not be using rotate(). We will use moveMotor () function
+}
+
+//Rotating the Z axis
+//EC == Encoder Value
+const int GEARREDUCTION = 5;
+
+int angleToEC (float angle){	//Returns the ec that it must move
+	return angle * GEARREDUCTION - getECValue();
+}
+
+//New Version of moveToTarget
+void moveToTarget (int targetEC, int tolerance){
+	int moveSpeed = 75;
+	//sigmoid function , PD control
+	bool cw  = true;
+	if (targetEC > fullRotation/2.0){
+		cw = false;
+		targetEC= fullRotation/2.0-targetEC;
+	}
+
+	rotate (cw, moveSpeed);
+
+	while ((fabs(getECValue())-fabs(targetEC)) <= tolerance);
+
+	rotate (false, 0);
+	//It will not be using rotate(). We will use moveMotor () function
+}
+
+//general btn function
+void waitBtn(){
+	while(nNxtButtonPressed==-1){}
+	while(nNxtButtonPressed!=-1){}
+}
+
+//---------------------------//
+//-----Read Point FileIO-----//
+//---------------------------//
 bool readNextPoint(Point &coor0){
 	coor0.x=0;
 	coor0.y=0;
@@ -31,22 +135,50 @@ bool readNextPoint(Point &coor0){
 	coor0.t=0;
 	return false;
 }
+//-----------------------------------------//
+//-----Some Trivial Functions for main-----//
+//-----------------------------------------//
+void message(int line, string sString){
+	int size=16;
+	int cnt=strLen(sString)/size;
+	for(int i=0;i<cnt;i++){
+		nxtDisplayString(line+i, sString);
+		StringDelete(sString, size*i, size);
+	}
+};
 //----------------------//
 //-----Main Program-----//
 //----------------------//
 task main()
 {
 	//---initialization---//
+  SensorType[S_ULTRA]=sensorSONAR;
+  SensorType[S4] = sensorI2CCustom9V;
 	//Check IF File open
 
-	//Zero Axes
+	//-BEGIN- Auto-Calibrating joint1
+  nxtDisplayCenteredTextLine(0, "Ready to Calibrate");
+  message(2, "Press Any Button to begin");
+  waitBtn();//Wait for starting the program
+  nxtDisplayCenteredTextLine(0, "Caution");
+  message(2,"Auto Calibration Starts in 2 seconds, Step back!");
+  PlaySound(soundBlip);
+  wait1Msec(2000);//Give 2 seconds for operator leaving the platform
+  ClearSounds();
+	zeroZAxis();
+	eraseDisplay();
+	//-END- of auto-calibration
 
+	//-BEGIN- start to performing
+	nxtDisplayCenteredTextLine(0, "System Ready");
+	message(2, "Press Any Button to begin the task");
+	waitBtn();
 	//Declare Variables
-	int QUANTITY;//first line from input file
+	int QUANTITY;//from first line from input file
 	int pointer=0;
 	int cnt=0;
 	Point coor[MAX];
-	Angle angle[MAX];
+	AngleSet angle[MAX];
 	//---READY---//
 	//Ready, Screen Output, Wait for button pressed
 
