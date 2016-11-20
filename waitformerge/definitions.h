f99c6749bc40e28677354274471451951c0e5649
Dustin Hu <DaShingHu@users.noreply.github.com>
2016-11-20 15:57:12 -0500

    Merged Oswaldo's
    Merged Oswaldo's code into here, updatetd list of definitions

--- waitformerge/definitions.h
+++ waitformerge/definitions.h
@@ -1,5 +1,15 @@
 
 // DO NOT USE RADIANSTODEGREES. IT CONVERTS IT TO A SHORT.
+const int S_COLOR = S1;
+const int S_ULTRA = S2;
+const int S_TOUCH = S3;
+const int S_SERVO = S4;
+const int J1 = motorA;
+const int J2 = 1;
+const int J3 = 2;
+const int GRIPPER = 3;
+const int MAX = 20;
+const int FULL_ROTATION_EC_VALUE = 360 * 5;
 const int SHOULDER = 157; // First length, from J2-J3 in mm
 const int FOREARM = 197; // Second length, from J3-End in mm
 const int Z_OFFSET = 112; // J1-J3 height in mm
@@ -26,6 +36,7 @@ typedef struct
 	float t;
 } AngleSet;
 
+// Dustin Hu
 bool calcAngleSet(Point& input, AngleSet& outputAngles);
 
 float calcL(Point& input);
@@ -43,3 +54,13 @@ void moveJ2(AngleSet& input);
 void moveJ3(AngleSet& input);
 
 float radToDeg(float rad);
+
+// Oswaldo Ferro Hernandez
+int getECValue();
+int getDistance();
+int angleToEC(float angle);
+
+void zeroECValue();
+void rotate(bool clockwise, int power);
+void moveToTarget(int targetEC, int tolerance);
+void zeroZAxis();
