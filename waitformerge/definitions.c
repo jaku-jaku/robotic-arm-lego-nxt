7cb134a6c27cd95b911d189f1ef76d9964274ee6
Dustin Hu <DaShingHu@users.noreply.github.com>
2016-11-20 16:37:22 -0500

    Added Ali's point validation
    De-obfuscation needed

--- waitformerge/definitions.c
+++ waitformerge/definitions.c
@@ -258,3 +258,42 @@ void zeroZAxis()
 	moveToTarget(targetEC, tolerance);
 	zeroECValue();	
 }
+
+bool isPointValid(Point p)
+{
+	float distBetween;
+	float pytho = sqrt(p.x*p.x + p.y*p.y);
+	bool isOk = false;
+	if (p.z > (-(L1+L2)*sin(PI/6.0)) && p.z < (L1+L2)*cos(PI/6.0))
+	{
+		distBetween = sqrt(pow(p.x,2)+pow(p.y,2)+pow(p.z,2));
+		if (distBetween > L1 && distBetween < (L1+L2))
+		{
+			isOk = true;
+		}
+	}
+	else if (p.z < (-(L1+L2)*sin(PI/6.0)))
+	{
+		distBetween = sqrt(pow(pytho-L1*cos(PI/6.0),2)+ pow(p.z-sin(PI/6.0),2));
+		if ((p.x >= 0 && p.x-L1*cos(PI/6.0) >= 0) || (p.x < 0 && p.x-L1*cos(PI/6.0) < 0))
+		{
+			if (distBetween < L2)
+			{
+				isOk = true;
+			}
+		}
+	}
+	else if (p.z > (L1+L2)*cos(PI/6.0))
+	{
+		
+		distBetween = sqrt(pow(pytho-L1*sin(PI/6.0),2)+ pow(p.z-cos(PI/6.0),2));
+		if ((p.x >= 0 && p.x-L1*sin(PI/6.0) >= 0) || (p.x < 0 && p.x-L1*sin(PI/6.0) < 0))
+		{
+			if (distBetween < L2)
+			{
+				isOk = true;
+			}
+		}
+	}
+	return isOk;
+}
