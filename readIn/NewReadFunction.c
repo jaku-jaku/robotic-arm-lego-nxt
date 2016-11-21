4c0fbd03d6f525608c8e6c73865d114e094ee52b
Ali Toyserkani <ali.toyserkani1998@gmail.com>
2016-11-21 16:57:12 -0500

    legibility update ;)

--- readIn/NewReadFunction.c
+++ readIn/NewReadFunction.c
@@ -9,18 +9,46 @@ typedef struct {
 const int L1 = 157;
 const int L2 = 197;
 
-// Check 
+// Check
+// (p.x >= 0 && p.x-L1*cos(PI/6.0) >= 0) || (p.x < 0 && p.x-L1*cos(PI/6.0) < 0)
+// (p.x >= 0 && p.x-L1*sin(PI/6.0) >= 0) || (p.x < 0 && p.x-L1*sin(PI/6.0) < 0) greater than upper bound check
+
+bool isUpperOrLowerInRange(bool isAbove, float x, ) {
+
+	bool isInRange = false;
+	if (isAbove)
+	{
+		if ((x >= 0 && x-L1*sin(PI/6.0) >= 0) || (x < 0 && x-L1*sin(PI/6.0) < 0))
+			isInRange = true;
+	}
+	else
+	{
+		if ((x >= 0 && x-L1*cos(PI/6.0) >= 0) || (x < 0 && x-L1*cos(PI/6.0) < 0))
+			isInRange = true;
+	}
+
+	return isInRange;
+
+
+}
 bool isPointValid(Point p)
 {
 	//Ali Toyserkani Nov 20
 	//Addition still needed, account for hitting itself within middle bound!!!!!!
 
+	const float lowerZBound = -(L1+L2)*sin(PI/6.0);
+	const float upperZBound = (L1+L2)*cos(PI/6.0);
+
 	float distJ3ToPoint;
 	float distBetween = sqrt(pow(p.x,2)+pow(p.y,2)+pow(p.z,2));
 	float pythoXY = sqrt(p.x*p.x + p.y*p.y);
 	float pythoL1L2 = sqrt(L1*L1 + L2*L2);
+
+	bool isUpper = false;
+
 	bool isOk = false;
-	if (p.z > (-(L1+L2)*sin(PI/6.0)) && p.z < (L1+L2)*cos(PI/6.0))
+
+	if (p.z > lowerZBound && p.z < upperZBound)
 	{
 		//distBetween = sqrt(pow(p.x,2)+pow(p.y,2)+pow(p.z,2));
 		if (distBetween > pythoL1L2 && distBetween < (L1+L2))
@@ -28,26 +56,26 @@ bool isPointValid(Point p)
 			isOk = true;
 		}
 	}
-	else if (p.z < (-(L1+L2)*sin(PI/6.0)))
+	else if (p.z < lowerZBound)
 	{
 		distJ3ToPoint = sqrt(pow(pythoXY-L1*cos(PI/6.0),2)+ pow(p.z-sin(PI/6.0),2));
-		
-		if ((p.x >= 0 && p.x-L1*cos(PI/6.0) >= 0) || (p.x < 0 && p.x-L1*cos(PI/6.0) < 0))
+
+		if (isUpperOrLowerInRange(false, p.x))
 		{
-			if (distBetween > pythoL1L2 && distJ2ToPoint < L2)
+			if (distBetween > pythoL1L2 && distJ3ToPoint < L2)
 			{
 				isOk = true;
 			}
 		}
 	}
-	else if (p.z > (L1+L2)*cos(PI/6.0))
+	else if (p.z > upperZBound)
 	{
-		
+
 		distJ3ToPoint = sqrt(pow(pythoXY-L1*sin(PI/6.0),2)+ pow(p.z-cos(PI/6.0),2));
-		
-		if ((p.x >= 0 && p.x-L1*sin(PI/6.0) >= 0) || (p.x < 0 && p.x-L1*sin(PI/6.0) < 0))
+
+		if (isUpperOrLowerInRange(true, p.x))
 		{
-			if (distBetween > pythoL1L2 && distJ2ToPoint < L2)
+			if (distBetween > pythoL1L2 && distJ3ToPoint < L2)
 			{
 				isOk = true;
 			}
@@ -77,11 +105,11 @@ task main()
 	if (!isFileOk)
 		displayString(0,"You fucked up");
 
-	Point filePoints[3];
-	
-	for (int i =0; i < 3; i++) {
+	Point filePoints[10];
+
+	for (int i =0; i < 5; i++) {
 		readPoint(fin,filePoints[i]);
-		displayString(0,"%f",filePoints[i].x);
+		displayString(0,"%d",filePoints[i].isValid);
 		wait1Msec(2000);
 	}
 
