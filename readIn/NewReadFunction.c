d2fb01068bf375933bfa8186970880048f4d0f18
Ali Toyserkani <ali.toyserkani1998@gmail.com>
2016-11-21 19:37:46 -0500

    Accounted for y when checking upper/lower

--- readIn/NewReadFunction.c
+++ readIn/NewReadFunction.c
@@ -2,8 +2,9 @@
 #include "NXT_FileIO.c"
 
 typedef struct {
-	float x,y,z;
+	float x,y,z, time;
 	bool isValid;
+	int gP;
 } Point;
 
 const int L1 = 157;
@@ -13,17 +14,17 @@ const int L2 = 197;
 // (p.x >= 0 && p.x-L1*cos(PI/6.0) >= 0) || (p.x < 0 && p.x-L1*cos(PI/6.0) < 0)
 // (p.x >= 0 && p.x-L1*sin(PI/6.0) >= 0) || (p.x < 0 && p.x-L1*sin(PI/6.0) < 0) greater than upper bound check
 
-bool isUpperOrLowerInRange(bool isAbove, float x) {
+bool isUpperOrLowerInRange(bool isAbove, float hyp,float x) {
 
 	bool isInRange = false;
 	if (isAbove)
 	{
-		if ((x >= 0 && x-L1*sin(PI/6.0) >= 0) || (x < 0 && x-L1*sin(PI/6.0) < 0))
+		if ((x >= 0 && hyp-L1*sin(PI/6.0) >= 0) || (x < 0 && hyp-L1*sin(PI/6.0) < 0))
 			isInRange = true;
 	}
 	else
 	{
-		if ((x >= 0 && x-L1*cos(PI/6.0) >= 0) || (x < 0 && x-L1*cos(PI/6.0) < 0))
+		if ((x >= 0 && hyp-L1*cos(PI/6.0) >= 0) || (x < 0 && hyp-L1*cos(PI/6.0) < 0))
 			isInRange = true;
 	}
 
@@ -51,6 +52,7 @@ bool isPointValid(Point p)
 	if (p.z > lowerZBound && p.z < upperZBound)
 	{
 		//distBetween = sqrt(pow(p.x,2)+pow(p.y,2)+pow(p.z,2));
+		displayString(1,"%d",p.z);
 		if (distBetween > pythoL1L2 && distBetween < (L1+L2))
 		{
 			isOk = true;
@@ -58,9 +60,10 @@ bool isPointValid(Point p)
 	}
 	else if (p.z < lowerZBound)
 	{
-		distJ3ToPoint = sqrt(pow(pythoXY-L1*cos(PI/6.0),2)+ pow(p.z-sin(PI/6.0),2));
+		displayString(1,"Lower Bound");
+		distJ3ToPoint = sqrt(pow(pythoXY - L1*cos(PI/6.0),2) + pow(p.z - L1*sin(PI/6.0),2));
 
-		if (isUpperOrLowerInRange(false, p.x))
+		if (isUpperOrLowerInRange(false, pythoXY, p.x ))
 		{
 			if (distBetween > pythoL1L2 && distJ3ToPoint < L2)
 			{
@@ -70,10 +73,11 @@ bool isPointValid(Point p)
 	}
 	else if (p.z > upperZBound)
 	{
-
-		distJ3ToPoint = sqrt(pow(pythoXY-L1*sin(PI/6.0),2)+ pow(p.z-cos(PI/6.0),2));
-
-		if (isUpperOrLowerInRange(true, p.x))
+		
+		distJ3ToPoint = sqrt(pow(pythoXY - L1*sin(PI/6.0),2) + pow(p.z - L1*cos(PI/6.0),2));
+		displayString(1,"%d",L1*cos(PI/6.0))
+		
+		if (isUpperOrLowerInRange(true, pythoXY, p.x))
 		{
 			if (distBetween > pythoL1L2 && distJ3ToPoint < L2)
 			{
@@ -87,9 +91,12 @@ bool isPointValid(Point p)
 
 void readPoint(TFileHandle & fin, Point p) {
 
+
 	readFloatPC(fin, p.x);
 	readFloatPC(fin, p.y);
 	readFloatPC(fin, p.z);
+	readFloatPC(fin,p.time);
+	readIntPC(fin,p.gP);
 
 	bool currPointValid = isPointValid(p);
 
@@ -100,19 +107,26 @@ void readPoint(TFileHandle & fin, Point p) {
 
 task main()
 {
-	TFileHandle fin;
-	bool isFileOk = openReadPC(fin,"test.txt");
-	if (!isFileOk)
-		displayString(0,"You fucked up");
+	//TFileHandle fin;
+	//bool isFileOk = openReadPC(fin,"test.txt");
+	//if (!isFileOk)
+	//	displayString(0,"You fucked up");
 
-	Point filePoints[10];
+	//Point filePoints[10];
 
-	for (int i =0; i < 5; i++) {
-		readPoint(fin,filePoints[i]);
-		displayString(0,"%d",filePoints[i].isValid);
-		wait1Msec(2000);
-	}
+	//for (int i =0; i < 10; i++) {
+	//	readPoint(fin,filePoints[i]);
+	//	displayString(1,"%d",filePoints[i].isValid);
+	//	displayString(2,"%d",i);
+	//	wait1Msec(2000);
+	//}
 
-	closeFilePC(fin);
+	//closeFilePC(fin);
+	
+	Point p1;
+	p1.x = 150; p1.y = 60; p1.z = 310;
+	p1.isValid = isPointValid(p1);
+	displayString(5,"%d",p1.isValid);
+	wait10Msec(1000);
 
 }
